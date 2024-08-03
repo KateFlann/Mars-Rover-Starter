3ad29fca562a849fb7c340b804bab5503cf05c33
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-08-03 13:36:35 -0400

    Mars Rover Assignment Commit 1

--- command.js
+++ command.js
@@ -1,12 +1,19 @@
 class Command {
-   constructor(commandType, value) {
-     this.commandType = commandType;
-     if (!commandType) {
-       throw Error("Command type required.");
-     }
-     this.value = value;
-   }
- 
- }
- 
- module.exports = Command;
+  constructor(commandType, value) {
+    this.commandType = commandType;
+    if (!commandType) {
+      throw Error("Command type required.");
+    }
+    this.value = value;
+  }
+}
+
+module.exports = Command;
+
+//TDD:
+//  Focus on one test at a time.
+// Write the test and then create the code to make it pass.
+// Only write the minimum amount of code needed to make the test pass.
+// There are some constraints on how you can implement these features.
+// Each numbered item describes a test. You should use the given phrases as the test descriptions when creating your it statements.
+// You must create 13 tests for this assignment.
