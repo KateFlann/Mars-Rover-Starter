06cf0411f466b7f9942234573ee53e5752c75959
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-08-03 17:11:42 -0400

    starting work on Rover section commit

--- rover.js
+++ rover.js
@@ -1,5 +1,36 @@
 class Rover {
-   // Write code here!
+  constructor(position) {
+    this.position = position;
+    this.mode = "NORMAL";
+    this.generatorWatts = 110;
+  }
+
+  receiveMessage(message) {
+    this.message = message;
+
+    let results = [];
+
+   
+    
+    let someObject = {
+      message: message.name,
+      results: message.commands
+    };
+  }
 }
+
+for (let message.commands[i] = 0; i < array.length; i++) {
+   if (message.command === "MOVE"){
+   position.push("completed: true")} else (
+
+   )
+   }
+ }
+
+
+STATUS_CHECK:
+MODE_CHANGE:
+
+
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -6,8 +6,72 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 
+// Test 7
+
 describe("Rover class", function() {
+ it("constructor sets position and default values for mode and generatorWatts", function(){
+  let position = new Rover("move 1");
+  expect(position.commandType).toBe("move 1");
+});
+ });
 
-  // 7 tests here!
 
+  // Test 8
+describe("Rover class", function() {
+  it("response returned by receiveMessage contains the name of the message", function(){
+   
+    });
+
+  // Test 9
+  describe("Rover class", function() {
+    it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+   
+    });
+
+ // Test 9
+ describe("Rover class", function() {
+  it("responds correctly to the status check command", function(){
+ 
+  });
+    
+
+// For the STATUS_CHECK command, receiveMessage(message).results includes a roverStatus object describing the current state of the rover object 
+// — mode, generatorWatts, and position. The test should check each of these for accuracy.
+// See the Rover Command Types table for more details.
+
+
+ // Test 10
+ describe("Rover class", function() {
+  it("responds correctly to the mode change command", function(){
+ 
+  });
+
+  // The test should check the completed property and rover mode for accuracy.
+// The rover has two modes that can be passed as values to a mode change command: ‘LOW_POWER’ and ‘NORMAL’.
+
+
+   // Test 11
+ describe("Rover class", function() {
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function(){
+ 
+  });
+
+// The test should check the completed property for accuracy and confirm that the rover’s position did not change.
+// Use the Rover Modes table for guidance on how to handle move commands in different modes.
+
+
+   // Test 12
+ describe("Rover class", function() {
+  it("responds with the position for the move command", function(){
+ 
+  });
+
+  // A MOVE command will update the rover’s position with the position value in the command.
+
+
+describe("Rover class", function () {
+  it("constructor sets rover type", function () {
+    let  = new Rover("move forward");
+    expect(forwardCommand.commandType).toBe("move forward");
+  });
 });
