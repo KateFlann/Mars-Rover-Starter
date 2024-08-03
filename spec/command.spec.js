7f1988a9ac4a828aed2cb561827e771570ba6a46
Amy <77540881+AmySofia1451@users.noreply.github.com>
2024-08-03 15:05:51 -0400

    Commit 2 with message and command classes completed

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -3,24 +3,24 @@ const Command = require("../command.js");
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-// describe("Command class", function () {
-//   it("throws error if command type is NOT passed into constructor as the first parameter", function () {
-//     expect(function () {
-//       new Command();
-//     }).toThrow(new Error("Command type required."));
-//   });
-// });
-
 describe("Command class", function () {
-  it("constructor sets command type", function () {
-    expect(typeof commandType).toBe("object");
+  it("throws error if command type is NOT passed into constructor as the first parameter", function () {
+    expect(function () {
+      new Command();
+    }).toThrow(new Error("Command type required."));
   });
 });
 
-// describe("Command class", function () {
-//   it("constructor sets a value passed in as the 2nd argument", function () {
-//     expect(function () {
-//       // constructor sets value property in object
-//     }).toThrow(new Error("Command type required."));
-//   });
-// });
+describe("Command class", function () {
+  it("constructor sets command type", function () {
+    let forwardCommand = new Command("move forward");
+    expect(forwardCommand.commandType).toBe("move forward");
+  });
+});
+
+describe("Command class", function () {
+  it("constructor sets a value passed in as the 2nd argument", function () {
+    let threeSpaces = new Command("move forward", "three spaces");
+    expect(threeSpaces.value).toBe("three spaces");
+  });
+});
